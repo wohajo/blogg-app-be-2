8e15932b651e8ca11db6eb063deee5455d807fd6
Michał Prawda <m.prawda99@gmail.com>
2021-01-10 17:11:00 +0100

    Initial commit

---
+++ src/main/java/com/prawda/demoBlogBE/DemoBlogBeApplication.java
@@ -0,0 +1,13 @@
+package com.prawda.demoBlogBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBlogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBlogBeApplication.class, args);
+	}
+
+}
